2e0de90f2f3cb5a63a05884f2785c1f4ce76c408
del089 <delir890@gmail.com>
2024-10-07 18:20:35 +0100

    quit the * cors

--- Infinity/infinity.py
+++ Infinity/infinity.py
@@ -29,12 +29,13 @@ app = FastAPI()
 
 origins = [
     "http://172.20.10.2:8080",  # La dirección de tu frontend Vuestic
+    "http://172.20.10.2,
     "http://localhost:8080",
+    "http://localhost",
     "http://80.233.48.17:8080", # ip publica de mi mac
     "http://80.233.48.17",
     "http://172.20.10.2", # ip de mi mac de mi red local
-    "http://172.20.10.15",
-    "*"
+    "http://172.20.10.15"
 ]
 
 
